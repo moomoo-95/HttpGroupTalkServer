9ecc4926249bb01efb0569d2b053d4eb854c7664
hyeonseong <gtws2081@daum.net>
2022-01-10 09:55:38 +0900

    1. test

---
+++ src/main/java/protocol/HgtpPacket.java
@@ -0,0 +1,14 @@
+package protocol;
+
+/**
+ *    The HGTP header has the following format:
+ *
+ *     0                   1                   2                   3
+ *     0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+ *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ *    Version
+ *    METHOD
+ */
+public class HgtpPacket {
+
+}
